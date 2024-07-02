0fc0383205cd78da4e05f1a8e30382c87c000fdc
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-02 15:47:25 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-/* Your Code Here */
+
+function createEmployeeRecord(employeeData) {
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(date) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(date.substring(11, 13)) * 100,
+        date: date.substring(0, 10),
+    });
+    return this;
+}
+
+function createTimeOutEvent(date) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(date.substring(11, 13)) * 100,
+        date: date.substring(0, 10),
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let time_in = this.timeInEvents.find(event => {
+        return event.date === date;
+    });
+    let time_out = this.timeOutEvents.find(event => {
+        return event.date === date;
+    });
+
+    return (time_out.hour - time_in.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => {
+        return firstName === employee.firstName;
+    })
+}
+
+function calculatePayroll(records) {
+    return records.reduce((accumulator, record) => {
+        return accumulator + allWagesFor.call(record);
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
